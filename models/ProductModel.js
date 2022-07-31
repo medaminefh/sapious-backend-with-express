4b4a42f648e2d9917abcd3356b54800e71ed97d2
Mohamed Amine Fhal <fhal.mohamed.amine@gmail.com>
2022-07-31 08:35:32 +0100

    change the quantity type to Number

--- models/ProductModel.js
+++ models/ProductModel.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const ProductSchema = mongoose.Schema({
   name: { type: String },
-  quantity: { type: String },
+  quantity: { type: Number },
 });
 
 export default mongoose.model("Product", ProductSchema);
